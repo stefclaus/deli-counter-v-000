8bdba5471ec00e667cb026d6e600732029c650e0
Stef Claus <stef.claus@gmail.com>
2018-09-14 00:49:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.count != 0 
+  katz_deli.each do |name, index|
+    line_count = index + 1 
+    puts "The line is currently #{line_count}. #{array[0]," 
+    end
+  return katz_deli 
+  else 
+    puts " currently empty."
+  end #ends the if 
+end #ends the function 
+
+
+def take_a_number(katz_deli, name)
+  puts 
+end 
+
+def now_serving(katz_deli)
+end 
