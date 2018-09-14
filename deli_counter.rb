3708e57ba5e1dabb1ee845536220d88263e2e962
Stef Claus <stef.claus@gmail.com>
2018-09-14 01:44:20 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,19 @@
 katz_deli = []
 
 def line(katz_deli)
+  new_array = []
   if katz_deli.count > 0 
-    katz_deli.join
-      puts "The line is currently 1. #{array[0]}" 
+    katz_deli.each_with_index do |name, index|
+      line_number = index + 1
+      line_order =  "."#{line_number} #{name}
+      new_array << line_order
+    end #ends iteration 
+    puts "The line is currently:" new_array
   else 
     puts "The line is currently empty."
   end #ends the if 
 end #ends the function 
+
 
 
 def take_a_number(katz_deli, name)
