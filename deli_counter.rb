8b02e13855c8ae90c0d238039d8d242048c68228
Stef Claus <stef.claus@gmail.com>
2018-11-15 03:18:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,33 +8,25 @@ def line(katz_deli)
     line_number = index + 1
     line_order =  "#{line_number}. #{name}"
     order << line_order
-    end #ends iteration
-    puts "The line is currently: #{line_order}.join(" ")"
-
-end
-
-  if katz_deli.count > 0
-    katz_deli.each_with_index do |name, index|
-    line_number = index + 1
-    line_order =  "#{line_number}. #{name}"
-    puts "The line is currently: #{line_order}"
-    end #ends iteration
-  else
+    end #ends iteration 
+    puts "The line is currently: #{line_order.join(" ")}"
+  else 
     puts "The line is currently empty."
-  end #ends the if
-end #ends the function
+  end #ends the if 
+end #ends the function 
 
 def take_a_number(katz_deli, name)
   line_number = katz_deli.count + 1
-  katz_deli << name
+  katz_deli << name 
   puts "Welcome, #{name}. You are number #{line_number} in line."
-end
+end 
 
 def now_serving(katz_deli)
   if katz_deli.count >= 1
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
-  else
+  else 
     puts "There is nobody waiting to be served!"
   end #ends if
-end
+end 
+  
